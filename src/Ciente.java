892e0167f9f53ec3c2eb1da22862ad896a70e928
Gabriel Paz <gabrieltecncontato@gmail.com>
2025-10-14 21:33:50 -0300

    Create Ciente.java

---
+++ src/Ciente.java
@@ -0,0 +1,14 @@
+// Classe que representa um cliente do banco
+public class Cliente {
+    private String nome; // Nome do cliente
+
+    // Construtor que define o nome do cliente
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    // Método para obter o nome do cliente
+    public String getNome() {
+        return nome;
+    }
+}
